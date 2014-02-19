25559d0f74be3b0fee6629c9893bba741e57e4d6
Adam Sunderman <amsunderman@gmail.com>
2014-02-19 17:09:56 -0600

    updated line length to 80

--- Project1/simp_util.h
+++ Project1/simp_util.h
@@ -32,28 +32,36 @@ struct simp_pixel {
 	unsigned char alpha_value;
 	};
 
-/* Function used to read from file fp and store it in the simp_file structure simp_data */
+/* Function used to read from file fp and store it in the simp_file structure 
+ * simp_data */
 int read_simp_file(FILE * fp, struct simp_file * simp_data);
 
-/* Function used to write to file fp using the simp data from the provided simp_file structure */
+/* Function used to write to file fp using the simp data from the provided 
+ * simp_file structure */
 int write_simp_file(FILE * fp, struct simp_file * simp_data);
 
-/* Function used to modify simp_file structure with crop coordinates and dimensions */
+/* Function used to modify simp_file structure with crop coordinates and 
+ * dimensions */
 int crop(struct simp_file * simp_data,struct simp_file * cropped, 
 	int x, int y, int width, int height);
 
-/* Function used to modify simp_file structure converting it to black and white */
+/* Function used to modify simp_file structure converting it to black and 
+ * white */
 int bw(struct simp_file * simp_data);
 
-/* Function used to modify simp_file structure by applying a color shift pattern */
+/* Function used to modify simp_file structure by applying a color shift 
+ * pattern */
 int colorshift(struct simp_file * simp_data, char * pattern);
 
-/* Function used to overlay an image on top of another image at coordinate x, y */
-int overlay(struct simp_file * simp_data, struct simp_file * overlay_data, int x, int y);
+/* Function used to overlay an image on top of another image at coordinate x, 
+ * y */
+int overlay(struct simp_file * simp_data, struct simp_file * overlay_data, 
+	int x, int y);
 
-/* Support function for overlay used to calculate RGB values with respect to their alpha values */
-void overlay_calculation(unsigned char * color_value1, unsigned char * color_value2,
-	unsigned char * alpha1, unsigned char * alpha2);
+/* Support function for overlay used to calculate RGB values with respect to 
+ * their alpha values */
+void overlay_calculation(unsigned char * color_value1, unsigned char * 
+	color_value2, unsigned char * alpha1, unsigned char * alpha2);
 
 /* Function used to free memory associated with simp_file data structure */
 int simp_data_clear(struct simp_file * simp_data);
