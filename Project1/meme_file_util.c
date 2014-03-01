595e63a3774b36da96c0b6bad2871dbf073ba8ca
Adam Sunderman <amsunderman@gmail.com>
2014-02-28 21:09:28 -0600

    finished tokenizing .mem file lines

--- Project1/meme_file_util.c
+++ Project1/meme_file_util.c
@@ -1,5 +1,8 @@
 #include "meme_file_util.h"
 
+#define START_TOKENS 5
+#define TOKEN_INCREASE_RATE 5
+
 /**Function used to read data stored in MEM file and store it in the meme_file
  * structure
  * @precondition meme_file * meme_data has already had memory allocated
@@ -16,7 +19,7 @@ int read_mem_file(FILE * fp, meme_file * meme_data)
 	int err, i = 0;
 
 	/*string to store current line*/
-	char * current_line;
+	char * current_line = NULL;
 
 	/*int to store the number of the current line*/
 	int line_count = 0;
@@ -133,7 +136,103 @@ int read_act_file(FILE * fp, action_file * action_data)
  * @modified 02/28/2014 */
 int mem_parse_line(char * left, char * right, meme_file * meme_data)
 {
-	/*TODO*/
+	/*strings used to tokenize left and right strings using " " as the
+ 	* delimiter*/
+	char** left_tokens = malloc(START_TOKENS * sizeof(char*));
+	char** right_tokens = malloc(START_TOKENS * sizeof(char*));
+
+	/*temp pointer for realloc*/
+	char** temp;
+
+	/*ints to store number of tokens in each input string*/
+	int left_num_tokens = 0;
+	int right_num_tokens = 0;
+
+	/*ints to store max number of tokens*/
+	int left_max_tokens = START_TOKENS;
+	int right_max_tokens = START_TOKENS;
+
+	/*counters*/
+	int i = 0, j = 0;
+
+	/*store initial left token*/
+	left_tokens[i] = strtok(left, " ");
+
+	/*tokenize left*/
+	while(left_tokens[i])
+	{
+		/*increment counters*/
+		i++;
+		left_num_tokens++;
+
+		/*if left_num_tokens is greater than our allocated memory we
+ 		* must re-allocate*/
+		if(left_num_tokens == left_max_tokens)
+		{
+			left_max_tokens += TOKEN_INCREASE_RATE;
+			temp = realloc(left_tokens, left_max_tokens * 
+				sizeof(char*));
+			if(!temp)
+			{
+				printf("mem_parse_line: failed to allocate " 
+					"memory\n");
+				return -1;
+			}
+			left_tokens = temp;
+		}
+
+		/*move onto next token (will be NULL if no more exist)*/
+		left_tokens[i] = strtok(NULL, " ");
+	}
+
+	/*store initial right token*/
+	right_tokens[j] = strtok(right, " ");
+
+	/*tokenize right*/
+	while(right_tokens[j])
+	{
+		/*increment counters*/
+		j++;
+		right_num_tokens++;
+
+		/*if right_num_tokens is greater than our allocated memory we
+ 		* must re-allocate*/
+		if(right_num_tokens == right_max_tokens)
+		{
+			right_max_tokens += TOKEN_INCREASE_RATE;
+			temp = realloc(right_tokens, right_max_tokens * 
+				sizeof(char*));
+			if(!temp)
+			{
+				printf("mem_parse_line: failed to allocate " 
+				"memory\n");
+				return -1;
+			}
+			right_tokens = temp;
+		}
+
+		/*move onto next token (will be NULL if no more exist)*/
+		right_tokens[j] = strtok(NULL, " ");
+	}
+
+	for(i = 0; i < left_num_tokens; i++)
+	{
+		printf("%s/", left_tokens[i]);
+	}
+	printf("\n");
+	for(j = 0; j < right_num_tokens; j++)
+	{
+		printf("%s/", right_tokens[j]);
+	}
+	printf("\n\n");
+
+	/*use left tokens to determine line type*/
+
+	/*TODO some other stuff*/
+
+	/*free memory*/
+	free(right_tokens);
+	free(left_tokens);
 
 	/*return successfully*/
 	return 0;
